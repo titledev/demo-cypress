18d8e33a75b46272d25162bf0f5628e64bd2ca79
titledev <title@hpktechnology.com>
2025-07-03 17:15:07 +0700

    Add Cypress reporting setup and update configuration
    - Added mochawesome and related packages for test reporting.
    - Updated Cypress configuration with base URL and reporter options.
    - Added script commands for merging and generating HTML reports.
    - Updated GitHub Actions workflow to upload test reports.
    - Added 'reports' directory to .gitignore.

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    // supportFile: "cypress/support/e2e.ts",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
 });
